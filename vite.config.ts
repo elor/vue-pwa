055cfa0e8096bae851311c9cf3e12e3d02f4ed6b
Erik E. Lorenz <erik@tuvero.de>
2023-02-19 10:12:01 +0100

    Revert "add vite-favicons-plugin to generate all required favicons (slow but works)"
    This reverts commit 7ee3e4f11076bbc97484022083a1290b7ea74b62.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
-import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +15,6 @@ export default defineConfig({
         skipWaiting: true,
       },
     }),
-    ViteFaviconsPlugin('./src/assets/logo.svg'),
   ],
   resolve: {
     alias: {
