7ee3e4f11076bbc97484022083a1290b7ea74b62
Erik E. Lorenz <erik@tuvero.de>
2023-02-19 10:07:58 +0100

    add vite-favicons-plugin to generate all required favicons (slow but works)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
+import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
         skipWaiting: true,
       },
     }),
+    ViteFaviconsPlugin('./src/assets/logo.svg'),
   ],
   resolve: {
     alias: {
